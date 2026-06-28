9b69537b69893598d3781660e5154030467bcfa7
schroneko <schroneko@users.noreply.github.com>
2026-06-28 11:03:19 +0900

    Add Homebrew cask definition

---
+++ Casks/nicevoice.rb
@@ -0,0 +1,24 @@
+cask "nicevoice" do
+  version "0.2.0"
+  sha256 "0a3146ca79b988f574aa5cd0559bf39784b96e1fdd289826dc7b05ea27cd73a6"
+
+  url "https://github.com/schroneko/nicevoice-app/releases/download/v#{version}/NiceVoice-#{version}.zip"
+  name "NiceVoice"
+  desc "Voice input app for macOS"
+  homepage "https://github.com/schroneko/nicevoice-app"
+
+  app "NiceVoice.app"
+
+  postflight do
+    system_command "/usr/bin/xattr",
+                   args: ["-dr", "com.apple.quarantine", "/Applications/NiceVoice.app"],
+                   sudo: false
+  end
+
+  uninstall quit: "app.nicevoice.NiceVoice"
+
+  zap trash: [
+    "~/Library/Logs/NiceVoice",
+    "~/Library/Preferences/app.nicevoice.NiceVoice.plist",
+  ]
+end
